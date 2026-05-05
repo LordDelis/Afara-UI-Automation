fd86bfd610fd06ac293247769d77719be79d84a2
LordDelis <anosikefidelis@gmail.com>
2026-05-05 06:02:57 +0100

    more update

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,8 +1,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-mochawesome-reporter/register';
-//import { slowCypressDown } from 'cypress-slow-down'
-//slowCypressDown()
 
 before(function () {
   cy.log('This should execute before all test cases')
@@ -20,9 +18,4 @@ afterEach(function () {
   cy.log('This should execute after each test case')
 })
 
-// Catch uncaught exceptions to prevent tests from failing unnecessarily
-// due to 3rd party scripts or minor app errors
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-})
+
